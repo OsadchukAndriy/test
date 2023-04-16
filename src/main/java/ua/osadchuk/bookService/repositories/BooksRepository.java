bd6111f23a4d7439488aca36a671bf1e8ec6b24f
Andrii Osadchuk <95745401+OsadchukAndriy@users.noreply.github.com>
2023-04-16 11:14:04 +0300

    terf

--- src/main/java/ua/osadchuk/bookService/repositories/BooksRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.osadchuk.bookService.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ua.osadchuk.bookService.models.Book;
-
-@Repository
-public interface BooksRepository extends JpaRepository<Book, Integer> {
-}
